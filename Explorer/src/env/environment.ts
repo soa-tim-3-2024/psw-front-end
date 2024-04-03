d15de125e97abaca582f2763a03b39f72d4dcf15
Bosko Kulusic <kulusicbosko@gmail.com>
2024-04-03 13:08:49 +0200

    https -> http

--- Explorer/src/env/environment.ts
+++ Explorer/src/env/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    host: "https://localhost:44333/",
-    apiHost: "https://localhost:44333/api/",
+    host: "http://localhost:44333/",
+    apiHost: "http://localhost:44333/api/",
     imageHost: "https://localhost:44333/images/",
     translateHost: "https://libretranslate.de/",
 };
